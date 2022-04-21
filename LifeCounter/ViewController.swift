c840fe49b09f4cdd103a2e33fc4934b533d1b9c6
Yeary02 <yyuan02@uw.edu>
2022-04-21 11:29:37 -0700

    finish

--- LifeCounter/ViewController.swift
+++ LifeCounter/ViewController.swift
@@ -23,53 +23,123 @@ class ViewController: UIViewController {
     var p1_lifeCount = 20
     var p2_lifeCount = 20
     
+    @IBOutlet weak var resultLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         p1_lifeCountLabel.text = "\(p1_lifeCount)"
         p2_lifeCountLabel.text = "\(p2_lifeCount)"
+        
+        if p1_lifeCount < 1 {
+            resultLabel.isHidden = false
+            resultLabel.text = "Player 1 LOSES!"
+        } else {
+            resultLabel.isHidden = true
+        }
+        
+        if p2_lifeCount < 1 {
+            resultLabel.isHidden = false
+            resultLabel.text = "Player 2 LOSES!"
+        } else {
+            resultLabel.isHidden = true
+        }
     }
     
     @IBAction func p1_minus1ButtonDidTouchUpInside(_ sender: Any) {
         p1_lifeCount -= 1
         p1_lifeCountLabel.text = "\(p1_lifeCount)"
+        
+        if p1_lifeCount < 1 {
+            resultLabel.isHidden = false
+            resultLabel.text = "Player 1 LOSES!"
+        } else {
+            resultLabel.isHidden = true
+        }
     }
     
     @IBAction func p1_minus5ButtonDidTouchUpInside(_ sender: Any) {
         p1_lifeCount -= 5
         p1_lifeCountLabel.text = "\(p1_lifeCount)"
+        
+        if p1_lifeCount < 1 {
+            resultLabel.isHidden = false
+            resultLabel.text = "Player 1 LOSES!"
+        } else {
+            resultLabel.isHidden = true
+        }
     }
     
     @IBAction func p1_plus1ButtonDidTouchUpInside(_ sender: Any) {
         p1_lifeCount += 1
         p1_lifeCountLabel.text = "\(p1_lifeCount)"
+        
+        if p1_lifeCount < 1 {
+            resultLabel.isHidden = false
+            resultLabel.text = "Player 1 LOSES!"
+        } else {
+            resultLabel.isHidden = true
+        }
     }
     
     @IBAction func p1_plus5ButtonDidTouchUpInside(_ sender: Any) {
         p1_lifeCount += 5
         p1_lifeCountLabel.text = "\(p1_lifeCount)"
+        
+        if p1_lifeCount < 1 {
+            resultLabel.isHidden = false
+            resultLabel.text = "Player 1 LOSES!"
+        } else {
+            resultLabel.isHidden = true
+        }
     }
     
     @IBAction func p2_minus1ButtonDidTouchUpInside(_ sender: Any) {
         p2_lifeCount -= 1
         p2_lifeCountLabel.text = "\(p2_lifeCount)"
+        
+        if p2_lifeCount < 1 {
+            resultLabel.isHidden = false
+            resultLabel.text = "Player 2 LOSES!"
+        } else {
+            resultLabel.isHidden = true
+        }
     }
     
     @IBAction func p2_minus5ButtonDidTouchUpInside(_ sender: Any) {
         p2_lifeCount -= 5
         p2_lifeCountLabel.text = "\(p2_lifeCount)"
+        
+        if p2_lifeCount < 1 {
+            resultLabel.isHidden = false
+            resultLabel.text = "Player 2 LOSES!"
+        } else {
+            resultLabel.isHidden = true
+        }
     }
     
     @IBAction func p2_plus1ButtonDidTouchUpInside(_ sender: Any) {
         p2_lifeCount += 1
         p2_lifeCountLabel.text = "\(p2_lifeCount)"
+        
+        if p2_lifeCount < 1 {
+            resultLabel.isHidden = false
+            resultLabel.text = "Player 2 LOSES!"
+        } else {
+            resultLabel.isHidden = true
+        }
     }
     
     @IBAction func p2_plus5ButtonDidTouchUpInside(_ sender: Any) {
         p2_lifeCount += 5
         p2_lifeCountLabel.text = "\(p2_lifeCount)"
+        
+        if p2_lifeCount < 1 {
+            resultLabel.isHidden = false
+            resultLabel.text = "Player 2 LOSES!"
+        } else {
+            resultLabel.isHidden = true
+        }
     }
-    
 }
 
